428026a9db84fdc8c43a9c456a978a05e4d71bc3
amurphy99 <amurphy62299@gmail.com>
2025-05-29 16:23:58 -0500

    run from home directory

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 set -e
 
-# Load logging helpers
-source ./utils/logging.sh
+# ====================================================================
+# Initial Setup
+# ====================================================================
+# Get path to the base directory (chat_app_deployment/../ = home/)
+SCRIPT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+UTILS_DIR="$SCRIPT_DIR/utils"
 
-# ====================================================================
+# Move up from chat_app_deployment to home
+#cd "$(dirname "$0")"
+#cd ..
+
+# Define path to the shell utils directory
+#SCRIPT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+#UTILS_DIR="$SCRIPT_DIR/utils"
+
+# Load logging helpers
+source "$UTILS_DIR/logging.sh"
+
 # First console output
-# ====================================================================
 echo -e "${BOLD_BLUE}$HR_2${RESET}"
 echo -e "${BOLD_BLUE} Starting Deployment of Speech System... ${RESET}"
 echo -e "${BOLD_BLUE}$HR_2${RESET}"
@@ -15,7 +28,7 @@ echo -e "${BOLD_BLUE}$HR_2${RESET}"
 # Load env config logic and pass args along
 # --------------------------------------------------------------------
 # This also does the basic config with github, log directories, etc...
-source ./utils/env_config.sh "$@" 
+source "$UTILS_DIR/env_config.sh" "$@" 
 
 # Echo the environment setup
 echo -e "${INFO_T0}ENV             = $ENV             ${RESET}"
@@ -29,23 +42,23 @@ echo -e "${INFO_T0}APP_ENVIRONMENT = $APP_ENVIRONMENT ${RESET}"
 # Setup Steps
 # ====================================================================
 # 1) Install Docker (Engine + Compose V2 Plugin)
-source ./utils/docker_utils/reset_docker.sh --step_num=0
-source ./utils/docker_utils/install_docker.sh --step_num=1
+source "$UTILS_DIR/docker_utils/reset_docker.sh" --step_num=0
+source "$UTILS_DIR/docker_utils/install_docker.sh" --step_num=1
 
 # 2) NVIDIA Setup (GPU Drivers, Container Toolkit)
-source ./utils/nvidia_gpu_setup.sh --step_num=2
+source "$UTILS_DIR/nvidia_gpu_setup.sh" --step_num=2
 
 # 3) Install System Dependencies (Git, Nginx, Certbot)
-source ./utils/install_dependencies.sh --step_num=3
+source "$UTILS_DIR/install_dependencies.sh" --step_num=3
 
 # 4) Clone/Pull Repo & Download deployment files from GCS Bucket
-source ./utils/download_files.sh --step_num=4
+source "$UTILS_DIR/download_files.sh" --step_num=4
 
 # 5) Configure Nginx & Run Certbot for HTTPS
-source ./utils/nginx_cert_config.sh --step_num=5
+source "$UTILS_DIR/nginx_cert_config.sh" --step_num=5
 
 # 6) Launch docker compose in headless mode
-source ./utils/launch_containers.sh --step_num=6
+source "$UTILS_DIR/launch_containers.sh" --step_num=6
 
 
 # ====================================================================
