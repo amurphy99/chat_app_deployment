6d43f726f8da3a435d09273f89e9dcb3e6bf4e5a
amurphy99 <amurphy62299@gmail.com>
2025-10-13 18:47:28 -0500

    always download

--- utils/download_files.sh
+++ utils/download_files.sh
@@ -38,7 +38,7 @@ echo -e "${INFO_T1}Download files from the GCS bucket...${RESET}"
 
 # Check if this has already been done by checking for an .env file
 # if [ ! -f "$MDL_DIR/new_LSA.csv" ]; then
-if [true]; then
+if true; then
     mkdir -p "$DPL_DIR"
     gsutil -m cp -r "$GCS_BUCKET/deployment-files/*" "$DPL_DIR/"
 else
