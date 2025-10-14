8a86824127cbd5336568ac375c90b6e495f5505b
amurphy99 <amurphy62299@gmail.com>
2025-10-13 19:24:08 -0500

    dont need google keys in frontend

--- utils/project_env.sh
+++ utils/project_env.sh
@@ -2,7 +2,7 @@
 # Configure Project Environment Variables
 # ==================================================================== ===================================
 echo -e "$PROG_HR_1"
-echo -e "${PROG_TEXT}4.5 Detailed project environment variable configuration... ${RESET}"
+echo -e "${PROG_TEXT}4. Detailed project environment variable configuration... ${RESET}"
 echo -e "$PROG_HR_2"
 echo "project_env.sh current working directory: $(pwd)"
 
@@ -11,11 +11,11 @@ echo -e "${INFO_T1}The project expects: [.env, frontend/.env, backend/.env] ${RE
 # ====================================================================
 # a) .env 
 # ====================================================================
-echo -e "${INFO_T2}Preparing root .env file...${RESET}"
+echo -e "\n${INFO_T2}Preparing root .env file...${RESET}"
 
 # File location
 ROOT_ENV_PATH="${APP_DIR}/.env"
-echo -e "${INFO_T0}ROOT_ENV_PATH = $ROOT_ENV_PATH ${RESET}"
+echo -e "${INFO_T3}ROOT_ENV_PATH = $ROOT_ENV_PATH ${RESET}"
 
 # Define some new environment variables
 VITE_RUN_ENV="PROD"
@@ -34,18 +34,18 @@ fi
 # Create the .env file
 cat <<EOF > $ROOT_ENV_PATH
 # Misc.
-APP_ENVIRONMENT=${APP_ENVIRONMENT}
-LLM_COMPOSE_FILE=${LLM_COMPOSE_FILE}
+APP_ENVIRONMENT  = ${APP_ENVIRONMENT}
+LLM_COMPOSE_FILE = ${LLM_COMPOSE_FILE}
 
 # Overrides (root .env file loaded second)
-VITE_RUN_ENV=${VITE_RUN_ENV}
-DEV_APP_ROUTE=${DEV_APP_ROUTE}
+VITE_RUN_ENV  = ${VITE_RUN_ENV}
+DEV_APP_ROUTE = ${DEV_APP_ROUTE}
 
 # Nginx & Certbot
-CONF_FILE=${CONF_FILE}
-DOMAIN=${DOMAIN}
-DOMAIN_WWW=${DOMAIN_WWW}
-CERT_EMAIL=${CERT_EMAIL}
+CONF_FILE  = ${CONF_FILE}
+DOMAIN     = ${DOMAIN}
+DOMAIN_WWW = ${DOMAIN_WWW}
+CERT_EMAIL = ${CERT_EMAIL}
 EOF
 
 echo -e "${GREEN}.env file created successfully${RESET}"
@@ -53,11 +53,11 @@ echo -e "${GREEN}.env file created successfully${RESET}"
 # ====================================================================
 # b) frontend/.env 
 # ====================================================================
-echo -e "${INFO_T2}Preparing frontend .env file...${RESET}"
+echo -e "\n${INFO_T2}Preparing frontend .env file...${RESET}"
 
 # File location
 FRONTEND_ENV_PATH="${APP_DIR}/frontend/.env"
-echo -e "${INFO_T0}FRONTEND_ENV_PATH = $FRONTEND_ENV_PATH ${RESET}"
+echo -e "${INFO_T3}FRONTEND_ENV_PATH = $FRONTEND_ENV_PATH ${RESET}"
 
 # --------------------------------------------------------------------
 # Create the .env file
@@ -72,10 +72,6 @@ VITE_API_URL    = "/api"
 VITE_RUN_ENV    = "DEV" 
 VITE_API_BASE   = "http://localhost:8000/api"
 VITE_WS_BASE    = "ws://localhost:8000/ws/chat/"
-
-GOOGLE_APPLICATION_CREDENTIALS = ${__GOOGLE_APPLICATION_CREDENTIALS}
-GOOGLE_API_KEY = ${__GEMINI_KEY}
-GOOGLE_GENAI_USE_VERTEXAI=0
 EOF
 
 echo -e "${GREEN}frontend/.env file created successfully${RESET}"
@@ -84,11 +80,11 @@ echo -e "${GREEN}frontend/.env file created successfully${RESET}"
 # ====================================================================
 # c) backend/.env 
 # ====================================================================
-echo -e "${INFO_T2}Preparing backend .env file...${RESET}"
+echo -e "\n${INFO_T2}Preparing backend .env file...${RESET}"
 
 # File location
 BACKEND_ENV_PATH="${APP_DIR}/backend/.env"
-echo -e "${INFO_T0}BACKEND_ENV_PATH = $BACKEND_ENV_PATH ${RESET}"
+echo -e "${INFO_T3}BACKEND_ENV_PATH = $BACKEND_ENV_PATH ${RESET}"
 
 # --------------------------------------------------------------------
 # Create the .env file
@@ -103,4 +99,3 @@ TZ                     = "UTC"
 EOF
 
 echo -e "${GREEN}backend/.env file created successfully${RESET}"
-
