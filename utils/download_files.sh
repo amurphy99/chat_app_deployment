0afd5316f2cfa440d68ac6f32a9d871e1f2ebdd8
amurphy99 <amurphy62299@gmail.com>
2026-02-12 18:33:03 -0600

    make it work on old instances

--- utils/download_files.sh
+++ utils/download_files.sh
@@ -78,6 +78,14 @@ else
     echo -e "${GREEN}Deployment files already exist locally, skipping download. ${RESET}"
 fi
 
+
+# Download the RAG embedding model folder
+if [ ! -f "$MDL_DIR/new_LSA.csv" ]; then
+    mkdir -p "$DPL_DIR" "$MDL_DIR" "$GSK_DIR"
+    gsutil -m cp -r "$GCS_BUCKET/deployment-files/models/MiniLM-L6-v2" "$DPL_DIR/"
+    mv "$DPL_DIR/MiniLM-L6-v2" "$MDL_DIR/"
+
+
 # Make a logs folder in deployment-files for persistence
 mkdir -p "$LOG_DIR"
 
