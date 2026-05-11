8fb13814a13d41adcdbc4dce6ece839d9b1550f7
amurphy99 <amurphy62299@gmail.com>
2026-05-11 12:09:45 -0500

    more colors

--- utils/logging.sh
+++ utils/logging.sh
@@ -3,14 +3,27 @@ echo "logging.sh current working directory: $(pwd)"
 # ================================================================================
 # Logging
 # ================================================================================
-RESET='\033[0m'
+RESET="\033[0m"
 
 # Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
+
+# Bright Colors
+BRIGHT_BLACK="\033[0;90m"
+BRIGHT_RED="\033[0;91m"
+BRIGHT_GREEN="\033[0;92m"
+BRIGHT_YELLOW="\033[0;93m"
+BRIGHT_BLUE="\033[0;94m"
+BRIGHT_MAGENTA="\033[0;95m"
+BRIGHT_CYAN="\033[0;96m"
+BRIGHT_WHITE="\033[0;97m"
 
 # Bold Colors
 BOLD_BLUE="\033[1;34m"
@@ -35,5 +48,5 @@ PROG_HR_2="${BLUE_H_LINE}"
 # Info formatting
 INFO_T0=$BOLD_YELL
 INFO_T1=$CYAN
-INFO_T2=$CYAN
+INFO_T2=$BRIGHT_BLUE
 INFO_T3=$YELLOW
