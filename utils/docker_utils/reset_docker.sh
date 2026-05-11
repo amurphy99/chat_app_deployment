b37796fb490ea2054c4ebea71c1064f6a212e2b3
amurphy99 <amurphy62299@gmail.com>
2026-05-11 11:24:01 -0500

    shut down existing containers first
    updated reset_docker to shut down existing containers first

--- utils/docker_utils/reset_docker.sh
+++ utils/docker_utils/reset_docker.sh
@@ -10,13 +10,30 @@ echo -e "${PROG_HR_2}"
 if ! command -v docker &>/dev/null; then
     echo -e "${GREEN}Docker not installed yet, nothing to clean up...${RESET}"
 else
-    #sudo docker-compose -f "$REPO_NAME/docker-compose.yaml" down || true
-    echo -e "${CYAN}Pruning unused Docker resources...${RESET}"
-    #sudo docker system prune -af --volumes=false || true
-    
-    echo "Pruning exited containers..."
+    # --------------------------------------------------------------------------------
+    # Shut down existing Docker containers from previous deployments
+    # --------------------------------------------------------------------------------
+    # This prevents old containers blocking ports needed for a new deployment
+    echo -e "${INFO_T1}Shutting down for existing Docker resources...${RESET}"
+
+    # Check if the `docker-compose` file exists yet (no need for this on the first install)
+    if [ -f "$REPO_NAME/docker-compose.yaml" ]; then
+        echo -e "Bringing down existing containers in ${INFO_T3}${REPO_NAME}${RESET}..."
+        sudo docker compose -f "$REPO_NAME/docker-compose.yaml" down || true
+    else
+        echo -e "No existing ${INFO_T3}${REPO_NAME}/docker-compose.yaml${RESET} found, skipping container teardown..."
+    fi
+
+    # --------------------------------------------------------------------------------
+    # Prune unused Docker resources (dangling containers and/or images)
+    # --------------------------------------------------------------------------------
+    echo -e "${INFO_T1}Pruning unused Docker resources...${RESET}"
+
+    # Containers
+    echo -e "Pruning exited containers..."
     sudo docker container prune -f
 
-    echo "Pruning dangling images..."
+    # Images
+    echo -e "Pruning dangling images..."
     sudo docker image prune -f
 fi
